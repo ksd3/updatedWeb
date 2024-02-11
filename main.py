296360d746bbf1de74e16a16db157ee7db2e40a9
Shaunak Joshi <shauwnak.joshi@gmail.com>
2024-02-11 07:31:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from vision import get_answer as get_vision_answer
 from stats import predict_claim_probability
 from llm import rag_answer
@@ -10,6 +11,13 @@ def main(image_path, stats_sample):
     
     # Execute vision model
     vision_output = get_vision_answer(image_path, device, 480)
+    
+    # Save vision model output to JSON file
+    vision_output_json_path = 'vision_output.json'
+    with open(vision_output_json_path, 'w') as json_file:
+        json.dump({'vision_output': vision_output}, json_file)
+    
+    print(f"Vision model output saved to {vision_output_json_path}")
     
     # Execute stats model
     # Convert string input to list of floats
@@ -26,11 +34,10 @@ def main(image_path, stats_sample):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python main_script.py <image_path> <txt> '<stats_sample>'")
+        print("Usage: python main_script.py <image_path> '<stats_sample>'")
         sys.exit(1)
-    else:
-        image_path = sys.argv[1]
-        text = sys.argv[2]
-        stats_sample = sys.argv[3]
-
-        main(image_path, stats_sample)
+    
+    image_path = sys.argv[1]
+    stats_sample = sys.argv[2]
+    
+    main(image_path, stats_sample)
